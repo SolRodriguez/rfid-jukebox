724fecb45bb4451bd2510b13aa196cec0d9ac4af
Madeline Loui <mloui@mit.edu>
2022-04-13 16:12:32 -0400

    Update rfid-jukebox.ino

--- rfid-jukebox.ino
+++ rfid-jukebox.ino
@@ -8,6 +8,8 @@ Uart mySerial (&sercom1, 2, 3, SERCOM_RX_PAD_1, UART_TX_PAD_0);
 
 RFID nano; //Create instance
 
+const int analogInPin2 = A2; 
+int sensorValue = 0; 
 
 void setup() {
   // Reassign pins 5 and 6 to SERCOM alt
@@ -24,5 +26,12 @@ Serial.println("setup");
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("loop");
+  // read the analog in value
+  sensorValue = analogRead(analogInPin2); 
+  // print the results to the Serial Monitor 
+  Serial.print("sensor = ");
+  Serial.println(sensorValue);
+  // wait 2 milliseconds before the next loop for the analog-to-digital 
+  // converter to settle after the last reading:
+  delay(2);
 }
